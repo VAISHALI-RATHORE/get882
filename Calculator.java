50f721d9aa5c0170898ecf4d90bcefc49e8d480c
VAISHALI-RATHORE <35846600+VAISHALI-RATHORE@users.noreply.github.com>
2020-12-17 00:09:42 -0800

    first commit
    created Calculator class and added main method.

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+class Calculator{
+    public static void add(int num1,num2){
+    return num1+num2'
+    }
+    
+    public static void main(String []args){
+      System.out.println(add(10,20));   
+    }
+
+}
