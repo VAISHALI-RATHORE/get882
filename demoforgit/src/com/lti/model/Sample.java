3f7e5fa6969577a649a327aaca2dff650eb436cc
vaishali <vaishali@192.168.0.6>
2020-12-17 17:06:12 -0800

    method added

--- demoforgit/src/com/lti/model/Sample.java
+++ demoforgit/src/com/lti/model/Sample.java
@@ -4,7 +4,9 @@ public class Sample {
 	public void show() {
 		System.out.println("hello");
 	}
-
+	public void method() {
+		System.out.println("my world");
+	}
 	public static void main(String[] args) {
 		
 
